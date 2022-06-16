106b04839a258acf830d9525c4e2e0cc8ea7eaed
Adam Žák <adamkozak393@gmail.com>
2022-06-16 09:53:01 +0200

    Update 1.0.2

--- lvl1/function.php
+++ lvl1/function.php
@@ -1,22 +1,23 @@
 <?php
 date_default_timezone_set("Europe/Bratislava");
-function neskoro(){
-$time = date("H:i");
-    if($time>="08"&&$time<="19:59"){
-        echo file_put_contents("log.txt", "Meskanie ".$time."\n", FILE_APPEND);
+function neskoro()
+{
+    $time = date("H:i");
+    if ($time >= "08" && $time <= "19:59") {
+        echo file_put_contents("log.txt", "Meskanie " . $time . "\n", FILE_APPEND);
     }
-    if($time>="20"&&$time<="24"){
+    if ($time >= "20" && $time <= "24") {
         die("Nemozne");
     }
-    function prichod(){
+}
+function prichod(){
     $time = date("H:i");
-        if($time<="07:59"){
-            echo file_put_contents("log.txt", "Prichod ".$time."\n", FILE_APPEND);
-        }
+    if($time<="07:59"){
+        echo file_put_contents("log.txt", "Prichod ".$time."\n", FILE_APPEND);
     }
-    function vypislogu(){
+}
+function vypislogu(){
         echo file_get_contents("log.txt.");
-    }
 }
 echo neskoro();
 echo prichod();
