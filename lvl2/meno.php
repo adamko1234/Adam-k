b6ee38c5e0e028e875b3e6acd0a913380958d6f3
Adam Žák <adamkozak393@gmail.com>
2022-06-22 11:28:38 +0200

    Update 1.0.1

--- lvl2/meno.php
+++ lvl2/meno.php
@@ -1,6 +1,32 @@
 <html>
 <body>
-Name: <?php echo $_POST["name"]; ?>
+<?php
+//echo $_POST["name"];
+date_default_timezone_set("Europe/Bratislava");
+function neskoro($data){
+    $time = date("H:i");
+    if($time >= "15" && $time <= "19:59"){
+    echo file_put_contents("log.txt", $data ." ". $time . "\n", FILE_APPEND);
+    }
+    if ($time >= "20" && $time <= "24") {
+        die("Nemozne");
+    }
+}
+function prichod($data){
+    $time = date("H:i");
+    if($time<="14:59"){
+        echo file_put_contents("log.txt", $data . " " . $time . "\n", FILE_APPEND);
+    }
+}
+function vypislogu($data){
+    echo file_get_contents("log.txt.",$data);
+}
+$data = "Meskanie:"." ". $_POST["name"];
+$data = "Prichod:". " ". $_POST["name"];
+echo neskoro($data);
+echo prichod($data);
+echo vypislogu($data);
+?>
 </body>
 </html>
 
