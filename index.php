a1711e121d13549187fa5e36f3fd56910dce4d1a
adamko1234 <94757283+adamko1234@users.noreply.github.com>
2023-01-19 09:18:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,48 +1,64 @@
-<?php
-include'meno.php';
-index();
-arrivals();
-students();
-note();
-
-function arrivals($jsonTime)
-{
-    $arrivals = include 'prichody.php';
-    $jsonTime = json_decode($arrivals);
-}
-function students($jsonStudent)
-{
-    $students = include 'studenti.php';
-    $jsonStudent = json_decode($students);
-}
-function index($jsonTime,$jsonStudent)
-{
+    <?php
     date_default_timezone_set("Europe/Bratislava");
-    $time = date("H:i");
-    $name = $_POST["name"];
-    if (empty($jsonStudent)) {
-        $jsonStudent = [$name];
-    } else {
-        array_push($jsonStudent, $name->putStudent());
+
+    if (isset($_POST["name"]))
+    {
+        $name = $_POST["name"];
+        $jsonTime = arrivals();
+        $jsonStudent = students();
+        index($name, $jsonTime, $jsonStudent);
+        echo note($jsonStudent, $jsonTime);
     }
-    if (empty($jsonTime)) {
-        $jsonTime = [$time];
-    } else {
-        array_push($jsonTime, $time->putTime());
+    function arrivals()
+    {
+        $arrivals = file_get_contents("prichody.json");
+        return json_decode($arrivals, true);
     }
-}
-function note($jsonStudent)
-{
-    foreach ($jsonStudent as $student) {
+
+    function students()
+    {
+        $students = file_get_contents("studenti.json");
+        return json_decode($students, true);
+    }
+
+    function index($name, &$jsonTime, &$jsonStudent)
+    {
         $time = date("H:i");
-        if ($time <= "07:59") {
-            echo "Prichod: " . $time . " => " . $student . "\n";
+        if (empty($jsonStudent))
+        {
+            $jsonStudent = [];
+            array_push($jsonStudent, $name);
         }
-        if ("08:00" <= $time & $time <= "20:00") {
-            echo "Meskanie: " . $time . " => " . $student . "\n";
+        if (empty($jsonTime))
+        {
+            $jsonTime = [];
+        array_push($jsonTime, $time);
         }
-        if ($time > "20" && $time <= "24") {
-            die($_POST['name'] . " Nemozne ");
-        }
+
+        saveData($jsonTime, $jsonStudent);
     }
-}
+
+    function saveData($jsonTime, $jsonStudent) {
+        file_put_contents("prichody.json", json_encode($jsonTime));
+        file_put_contents("studenti.json", json_encode($jsonStudent));
+    }
+
+    function note($jsonStudent, $jsonTime)
+    {
+        $notes = "";
+        foreach ($jsonStudent as $index => $student) {
+            if(isset($jsonTime[$index])){
+                $time = $jsonTime[$index];
+                if ($time <= "07:59") {
+                    $notes .= "Prichod: " . $time . " => " . $student . "<br> ";
+                }
+                if ("08:00" <= $time & $time <= "20:00") {
+                    $notes .= "Meskanie: " . $time . " => " . $student . "<br> ";
+                }
+                if ($time > "20" && $time <= "24") {
+                    die($_POST['name'] . " Nemozne ");
+                }
+            }
+        }
+        return $notes;
+    }
